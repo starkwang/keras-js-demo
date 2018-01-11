1f42dacf8462c7db69c64c9c8d33ec3216587c23
Weijia Wang <381152119@qq.com>
2018-01-11 22:05:05 +0800

    dist

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/keras-js-demo',
+    assetsPublicPath: '/keras-js-demo/dist',
 
     /**
      * Source Maps
